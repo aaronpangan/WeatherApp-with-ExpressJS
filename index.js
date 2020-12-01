7b7c20f0dedcf99650259f4b78ca1b3ba734271d
aaronjoshua27 <59806229+aaronjoshua27@users.noreply.github.com>
2020-12-02 05:19:29 +0800

    Time Experiment
    Added Git Ignore
    Added City Time, Humidity,Temperature
     ,Wind, Local Weather

--- index.js
+++ index.js
@@ -1,21 +1,79 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-
+const bodyparser = require('body-parser');
+const geoTz = require('geo-tz');
+const d2d = require('degrees-to-direction');
 app.set('view engine', 'ejs');
 
-app.use('/', async (req, res) => {
+app.use(bodyparser.urlencoded({ extended: false }));
+
+// For rendering the Input Field
+app.get('/', async (req, res) => {
+  let convert = Intl.DateTimeFormat().resolvedOptions().timeZone + '';
+
+  convert = convert.split('/');
+
   let data = await fetch(
-    'https://api.openweathermap.org/data/2.5/weather?q=manilasds&appid=b89b3cb176bf02ce436c0ec42e9973fe'
+    `https://api.openweathermap.org/data/2.5/weather?q=${convert[1]}&appid=b89b3cb176bf02ce436c0ec42e9973fe`
   );
 
   data = await data.json();
 
-  if ((data.cod = 404)) return res.status(404).send('Invalid City, Try Again');
-  console.log(data.cod);
+  let timezone = geoTz(data.coord.lat, data.coord.lon);
+  let date =
+    new Date().toLocaleString('en-US', {
+      timeZone: timezone,
+    }) + '';
+
+  const item = {
+    desc: 'This is your weather',
+    mainWeather: data.weather[0].main,
+    weatherDesc: data.weather[0].description,
+    currentTemp: `${data.main.temp} Degrees Celsius`,
+    humidity: `${data.main.humidity}% Humidity`,
+    pressure: `${data.main.pressure} hPa`,
+    wind: `${data.wind.speed}m/s ${d2d(data.wind.deg)}`,
+    time: date,
+  };
 
   console.log(data);
-  res.send(data);
+
+  res.render('index', { item });
+});
+
+// To show the result
+app.post('/', async (req, res) => {
+  let data = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=b89b3cb176bf02ce436c0ec42e9973fe`
+  );
+
+  data = await data.json();
+
+  // If the input is invalid
+  if (data.cod != 200) return res.status(404).send('Invalid City, Try Again');
+
+  let timezone = geoTz(data.coord.lat, data.coord.lon);
+
+  let date =
+    new Date().toLocaleString('en-US', {
+      timeZone: timezone,
+    }) + '';
+
+  const item = {
+    desc: `You search for ${req.body.city}`,
+    mainWeather: data.weather[0].main,
+    weatherDesc: data.weather[0].description,
+    currentTemp: `${data.main.temp} Degrees Celsius`,
+    humidity: `${data.main.humidity}% Humidity`,
+    pressure: `${data.main.pressure} hPa`,
+    wind: `${data.wind.speed}m/s ${d2d(data.wind.deg)}`,
+    time: date,
+  };
+  console.log(date);
+  console.log(timezone);
+  // Render index.ejs with the result
+  res.render('index', { item });
 });
 
 const port = process.env.PORT || 3000;
@@ -24,4 +82,4 @@ app.listen(port, () => {
   console.log(`Server started at localhost:${port}`);
 });
 
-// res.render('index', { items: items });
+['Asia', 'Manila'];
---
+++ sample.js
@@ -0,0 +1,34 @@
+{
+    coord: { lon: 120.98, lat: 14.6 },
+    weather: [
+      {
+        id: 802,
+        main: 'Clouds',
+        description: 'scattered clouds',
+        icon: '03n'
+      }
+    ],
+    base: 'stations',
+    main: {
+      temp: 298.64,
+      feels_like: 300.76,
+      temp_min: 298.15,
+      temp_max: 299.15,
+      pressure: 1007,
+      humidity: 74
+    },
+    visibility: 10000,
+    wind: { speed: 2.6, deg: 360 },
+    clouds: { all: 40 },
+    dt: 1606852196,
+    sys: {
+      type: 1,
+      id: 8160,
+      country: 'PH',
+      sunrise: 1606860366,
+      sunset: 1606901117
+    },
+    timezone: 28800,
+    id: 1701668,
+    name: 'Manila',
+    cod: 200
