917dae16f158f4b6ef8dbea34ddd7f4b305d6c2c
aaronjoshua27 <59806229+aaronjoshua27@users.noreply.github.com>
2020-12-03 04:42:40 +0800

    Third Commit
    Finishes Backend
    Added Simple design

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
 const express = require('express');
+// For Fetching Api
 const fetch = require('node-fetch');
 const app = express();
 const bodyparser = require('body-parser');
+
+// For getting the timezone based on latitude, longtitude
 const geoTz = require('geo-tz');
-const d2d = require('degrees-to-direction');
+
+// to assign the values to an object
+const processData = require('./processData');
+
 app.set('view engine', 'ejs');
 
 app.use(bodyparser.urlencoded({ extended: false }));
 
-// For rendering the Input Field
+// Getting the weather based on your location
 app.get('/', async (req, res) => {
+  // For getting your timezone
   let convert = Intl.DateTimeFormat().resolvedOptions().timeZone + '';
 
   convert = convert.split('/');
 
+  // Fetching current weather in your location
   let data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${convert[1]}&appid=b89b3cb176bf02ce436c0ec42e9973fe`
   );
 
   data = await data.json();
 
+  // Getting the your time
   let timezone = geoTz(data.coord.lat, data.coord.lon);
   let date =
     new Date().toLocaleString('en-US', {
       timeZone: timezone,
     }) + '';
 
-  const item = {
-    desc: 'This is your weather',
-    mainWeather: data.weather[0].main,
-    weatherDesc: data.weather[0].description,
-    currentTemp: `${data.main.temp} Degrees Celsius`,
-    humidity: `${data.main.humidity}% Humidity`,
-    pressure: `${data.main.pressure} hPa`,
-    wind: `${data.wind.speed}m/s ${d2d(data.wind.deg)}`,
-    time: date,
-  };
+  // Arranging the values to processData.js then assign to a variable
+  const item = processData('This is your current weather', data, date);
 
   console.log(data);
 
+  // Render the data to ejs file
   res.render('index', { item });
 });
 
-// To show the result
+// To show the result of your searched city
 app.post('/', async (req, res) => {
+  // Getting the api based on your searched city
   let data = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&appid=b89b3cb176bf02ce436c0ec42e9973fe`
   );
@@ -53,33 +56,25 @@ app.post('/', async (req, res) => {
   // If the input is invalid
   if (data.cod != 200) return res.status(404).send('Invalid City, Try Again');
 
+  // Getting the timezone of that city
   let timezone = geoTz(data.coord.lat, data.coord.lon);
 
+  // Getting the timezone of that city
   let date =
     new Date().toLocaleString('en-US', {
       timeZone: timezone,
     }) + '';
 
-  const item = {
-    desc: `You search for ${req.body.city}`,
-    mainWeather: data.weather[0].main,
-    weatherDesc: data.weather[0].description,
-    currentTemp: `${data.main.temp} Degrees Celsius`,
-    humidity: `${data.main.humidity}% Humidity`,
-    pressure: `${data.main.pressure} hPa`,
-    wind: `${data.wind.speed}m/s ${d2d(data.wind.deg)}`,
-    time: date,
-  };
-  console.log(date);
-  console.log(timezone);
+  // Arranging the values to processData.js then assign to a variable
+  const item = processData(`You searched for ${req.body.city}`, data, date);
+
   // Render index.ejs with the result
   res.render('index', { item });
 });
 
+// Arranging the server
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server started at localhost:${port}`);
 });
-
-['Asia', 'Manila'];
---
+++ processData.js
@@ -0,0 +1,21 @@
+const d2d = require('degrees-to-direction');
+
+module.exports = (place, data, date) => {
+  // Assinging the data to a single object
+
+  const item = {
+    desc: place,
+    country: data.sys.country,
+    mainWeather: `${data.weather[0].main}`,
+    weatherDesc: `${data.weather[0].description}`,
+    currentTemp: `${(data.main.temp - 272.15).toFixed(2)}`,
+    maxTemp: `${(data.main.temp_max - 272.15).toFixed(2)}`,
+    minTemp: `${(data.main.temp_min - 272.15).toFixed(2)}`,
+    humidity: `${data.main.humidity}%`,
+    pressure: `${data.main.pressure} hPa`,
+    wind: `${data.wind.speed}m/s ${d2d(data.wind.deg)}`,
+    time: `${date}`,
+  };
+
+  return item;
+};
